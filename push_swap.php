8d823b617542fa1db31938bb37c9319df7a25579
wac <daisy.bruneau@epitech.eu>
2020-05-14 19:07:00 +0200

    .

--- push_swap.php
+++ push_swap.php
@@ -19,7 +19,10 @@
 //         case "sa" : sort($lb[1], $lb[2]);
 //     }
 // }
+// echo "ceci est mon argv" . print_r($argv) . PHP_EOL;
 
+$la = [];
+$lb = [];
 
 foreach ($argv as $key => $value) {
 
@@ -28,19 +31,54 @@ foreach ($argv as $key => $value) {
     for ($key; $key >= 1; $key++) 
     { 
         echo "[$key] -> $value" . PHP_EOL;
-
+        
         // if($key > 0)
         // {
-        //     $key++;
-        //     print_r($key);
-        // }
-
-        // $key++;
-        // echo $key;
-
+            //     $key++;
+            //     print_r($key);
+            // }
+            
+            // $key++;
+            // echo $key;
+            
         break;
     }
-    // echo $value . PHP_EOL;
+    
+    
 }
 
+if (count($la) > 1)
+{
+    $count = count($la);
+    if($la === sort($lb))
+    {
+        push();
+    }
+}
+
+
+
+function push()
+{
+
+}
+
+
+function swap()
+{
+
+}
+
+function rotate()
+{
+    
+}
+
+// $newtable = array_fill(1,$key,$value);
+// var_dump($newtable);
+//     sort($newtable);
+    // print_r($newtable);
+
+
+// echo $value . PHP_EOL;
 // var_dump($i);
